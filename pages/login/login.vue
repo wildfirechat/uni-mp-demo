0e1ec9c7a720c9c981ca1631fa83e6efbc8fa110
x86 <1960867973@qq.com>
2021-09-14 15:43:48 +0800

    默认使用uniapp平台

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -83,7 +83,7 @@ export default {
             if (loginResult.code === 0) {
               let userId = loginResult.result.userId;
               let token = loginResult.result.token;
-              wfc.connect(userId, token, 'ali');
+              wfc.connect(userId, token, 'uni');
               wx.switchTab({
                 url: '../chat-list/chat-list',
                 fail: e => {
